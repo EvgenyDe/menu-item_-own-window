2e8eb18aaa0672514a551b33f07ac75e558ac4fe
Evgeny Denisovets <evgenyde@playtika.com>
2022-02-02 21:56:43 +0300

    texture works

--- Assets/Scripts/MyMakerTexture.cs
+++ Assets/Scripts/MyMakerTexture.cs
@@ -8,17 +8,21 @@ using Random = UnityEngine.Random;
 
 public class MyMakerTexture : EditorWindow
 {
+    public GameObject cube;
+    
     private  Color[,] pixels = new Color[8,8];
 
     private Rect[,] rects = new Rect[8, 8];
 
-    private Texture[] _textures = new Texture[8 * 8];
+    private Texture2D outputTexture;
     
     private Color _colorForLeftClick =Color.red;
     private Color _colorForRightCkick =Color.yellow;
 
+    
     private void OnEnable()
     {
+        
         for (int i = 0; i < pixels.GetLongLength(0); i++)
         {
             for (int j = 0; j < pixels.GetLongLength(0); j++)
@@ -26,6 +30,10 @@ public class MyMakerTexture : EditorWindow
                 pixels[i, j]=RandomColor();
             }
         }
+        
+        outputTexture = new Texture2D(8, 8);
+        
+        
     }
 
     [MenuItem("MyTOOLS/MakerTexture")]
@@ -64,14 +72,60 @@ public class MyMakerTexture : EditorWindow
             }
             GUILayout.EndHorizontal();
         }
-        
+
+        MakeTexture();
         // if(evnt.type == EventType.MouseDown&&)
         //
         GUILayout.BeginVertical();
         _colorForLeftClick = EditorGUILayout.ColorField(_colorForLeftClick);
         _colorForRightCkick = EditorGUILayout.ColorField(_colorForRightCkick);
+
+        outputTexture = (Texture2D) EditorGUILayout.ObjectField("Output Texture", outputTexture, typeof(Texture2D), true);
+        cube = (GameObject) EditorGUILayout.ObjectField("Target Object", cube, typeof(GameObject),true);
+        
+        if (GUILayout.Button("Set One Color"))
+        {
+            Debug.Log("Apply texture to object");
+            SetOneColor();
+        }
+        if (GUILayout.Button("Apply texture to object"))
+        {
+            Debug.Log("Apply texture to object");
+            SetTextureToCub();
+        }
+        
+        
+        
+        
         GUILayout.EndVertical();
 
+    }
+
+    private void SetOneColor()
+    {
+       // cube.GetComponent<MeshRenderer>().material.color = _colorForLeftClick;
+    }
+
+    private void SetTextureToCub()
+    {
+        cube.GetComponent<MeshRenderer>().sharedMaterial.mainTexture=outputTexture;
+    }
+
+    private void MakeTexture()
+    {
+        outputTexture = new Texture2D(8, 8);
+        
+        for (int y = 0; y < outputTexture.height; y++)
+        {
+            for (int x = 0; x < outputTexture.width; x++)
+            {
+                outputTexture.SetPixel(x, y, pixels[x,y]);
+            }
+        }
+        outputTexture.Apply();
+        
+        //cube.GetComponent<MeshRenderer>().material.mainTexture = outputTexture;
+        
     }
 
     private void CheckLastBox(int i, int j)
